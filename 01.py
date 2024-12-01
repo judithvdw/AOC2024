3355f55cb9a94c1b872de14fd9702fff16beaa4e
Judith <judith@jmvdw.nl>
2024-12-01 08:01:29 +0100

    day 1

---
+++ 01.py
@@ -0,0 +1,10 @@
+from collections import Counter
+
+with open("inputs/01.txt") as f:
+    data = [map(int, line.split()) for line in f.read().splitlines()]
+    transposed = [list(i) for i in zip(*data)]
+
+left, right = transposed
+counts = Counter(right)
+print(f"Part 1: {sum([abs(l - r) for l, r in zip(sorted(left), sorted(right))])}")
+print(f"Part 2: {sum([l * counts[l] for l in left])}")
