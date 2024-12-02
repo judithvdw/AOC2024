1f8573897df775d5c7502c194db504f5b433eb12
Judith <judith@jmvdw.nl>
2024-12-02 16:43:30 +0100

    Day 2

--- 02.py
+++ 02.py
@@ -6,11 +6,19 @@ def diffs(report):
     return [a - b for a, b in pairwise(report)]
 
 
-def is_valid(report):
-    return (min(report) >= -3 and max(report) < 0) or (min(report) > 0 and max(report) <= 3)
+def is_valid(diff):
+    return (min(diff) >= -3 and max(diff) < 0) or (min(diff) > 0 and max(diff) <= 3)
+
+
+def dampening(report):
+    for i in range(len(report)):
+        if is_valid(diffs(report[:i] + report[i + 1:])):
+            return True
+    return False
 
 
 with open('inputs/02.txt') as f:
     reports = parse_ints(f.readlines())
 
-print(sum([is_valid(diffs(report)) for report in reports]))
+print(f"Part 1: {sum([is_valid(diffs(report)) for report in reports])}")
+print(f"Part 2: {sum([dampening(report) for report in reports])}")
---
+++ utils/parsing.py
@@ -0,0 +1,15 @@
+import re
+
+
+def parse_sparse_grid(lines, character="#"):
+    d = set()
+    for i, line in enumerate(lines):
+        for j, num in enumerate(line.strip()):
+            if num == character:
+                d.add((i, j))
+    return d
+
+
+def parse_ints(lines, negative=False):
+    pattern = r'-?\d+' if negative else r'\d+'
+    return [list(map(int, re.findall(pattern, line))) for line in lines]
