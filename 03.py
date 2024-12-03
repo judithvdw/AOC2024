31e52cfeca764b0cbb2c2ef85fe18e9886e5c16f
Judith <judith@jmvdw.nl>
2024-12-03 14:34:10 +0100

    Day 3

--- 03.py
+++ 03.py
@@ -3,16 +3,17 @@ from bisect import bisect
 from math import prod
 
 
-def clean_and_mul(s):
+def mul(s):
     return prod((map(int, s.split(","))))
 
 
 with open("inputs/03.txt") as f:
     code = f.read()
 
-muls = [(int(m.span()[0]), clean_and_mul(m.group(1))) for m in re.finditer(r'mul\((\d+,\d+)\)', code)]
+pattern = r'mul\((\d+,\d+)\)'
+muls = [(int(m.span()[0]), mul(m.group(1))) for m in re.finditer(pattern, code)]
 dos = [0] + [do.span()[0] for do in re.finditer(r'do\(\)', code)]
-donts = [-1] + [dont.span()[0] for dont in re.finditer(r'don\'t\(\)', code)]
+donts = [dont.span()[0] for dont in re.finditer(r'don\'t\(\)', code)]
 
 print(sum(a[1] for a in muls))
 print(sum(mul for loc, mul in muls if dos[bisect(dos, loc) - 1] > donts[bisect(donts, loc) - 1]))
