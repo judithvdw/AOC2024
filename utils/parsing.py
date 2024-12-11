130fec59d77769362c7f9ced2ef5a1c67f891a03
Judith <judith@jmvdw.nl>
2024-12-11 16:36:47 +0100

    Day 10

--- utils/parsing.py
+++ utils/parsing.py
@@ -16,10 +16,14 @@ def parse_ints(lines, negative=False):
     return [list(map(int, re.findall(pattern, line))) for line in lines]
 
 
-def parse_grid_v2(lines, skip=""):
+def parse_grid_v2(lines, skip="", nums=False):
     d = defaultdict(set)
     for y, line in enumerate(lines):
         for x, c in enumerate(line.strip()):
             if c not in skip:
-                d[c].add((x, y))
+                if nums:
+                    d[int(c)].add((x, y))
+                else:
+                    d[c].add((x, y))
+
     return d
