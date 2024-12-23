b7e5c7debb8b40b425c6172cbb49785e9282c4a0
Judith <judith@jmvdw.nl>
2024-12-23 12:48:18 +0100

    cleanup day 23

--- 23.py
+++ 23.py
@@ -1,13 +1,13 @@
 import networkx as nx
 
-with open("inputs/23.txt") as f:
-    edges = [tuple(x.strip().split("-")) for x in f.readlines()]
+with open('inputs/23.txt') as f:
+    edges = [tuple(x.strip().split('-')) for x in f.readlines()]
 
 graph = nx.Graph()
 graph.add_edges_from(edges)
 
-part1 = list(x for x in nx.enumerate_all_cliques(graph) if len(x) == 3 and "t" in "".join(y[0] for y in x))
-print(len(part1))
+part1 = list(x for x in nx.enumerate_all_cliques(graph) if len(x) == 3 and 't' in ''.join(y[0] for y in x))
+print(f'Part 1: {len(part1)}')
 
 part2 = nx.find_cliques(graph)
-print(",".join(sorted(max(list(part2), key=len))))
+print(f'Part 2: {','.join(sorted(max(list(part2), key=len)))}')
